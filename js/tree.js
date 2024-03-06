badf82ec878b2fdfc4d1cfd4e8fc6345c1458c16
opDare <100951720+opDare@users.noreply.github.com>
2024-03-06 17:39:36 +0800

    Update tree.js

--- js/tree.js
+++ js/tree.js
@@ -19,15 +19,29 @@ addNode("blank", {
 addLayer("tree-tab", {
     tabFormat: [
         ["tree", function() {return (layoutInfo.treeLayout ? layoutInfo.treeLayout : TREE_LAYERS)}],
+        ["clickable", 991],
         ["clickables", [1]] 
     ],
     previousTab: "",
     leftTab: true,
     clickables: {
+        991: {
+            title: "Mobile shift",
+            onClick() {
+                player.shift = !player.shift
+                shiftDown = player.shift
+            },
+            canClick() {return true},
+            unlocked() {return isMobile()},
+            style() {return{
+                'height': '90px',
+	            'width': '90px'
+            }}
+        },
         11: {
             title: "Hi reset",
             display() {
-                let desc = `<h3>(+${formatWhole(getResetGain('h'))})`
+                let desc = `<h3>(+${formatWhole(getResetGain('h'))})<br>Total: ${formatWhole(player.h.points)}`
                 return desc
             },
             onClick() {if(canReset('h')) doReset('h')},
@@ -42,13 +56,13 @@ addLayer("tree-tab", {
             display() {
                 let gain = getResetGain('k')
                 if (!canReset('k')) gain = zero
-                let desc = `<h3>(+${formatWhole(gain)})`
+                let desc = `<h3>(+${formatWhole(gain)})<br>Total: ${formatWhole(player.k.points)}`
                 return desc
             },
             onClick() {if(canReset('k')) doReset('k')},
             onHold() {if(canReset('k')) doReset('k')},
             canClick() {return true},
-	    unlocked() {return tmp.k.layerShown},
+            unlocked() {return tmp.k.layerShown},
             style() {return {
                 'background-color': tmp.k.color
             }}
@@ -56,13 +70,13 @@ addLayer("tree-tab", {
         13: {
             title: "Power reset",
             display() {
-                let desc = `<h3>(+${formatWhole(getResetGain('p'))})`
+                let desc = `<h3>(+${formatWhole(getResetGain('p'))})<br>Total: ${formatWhole(player.p.points)}`
                 return desc
             },
             onClick() {if(canReset('p')) doReset('p')},
             onHold() {if(canReset('p')) doReset('p')},
             canClick() {return true},
-	    unlocked() {return tmp.p.layerShown},
+            unlocked() {return tmp.p.layerShown},
             style() {return {
                 'background-color': tmp.p.color
             }}
